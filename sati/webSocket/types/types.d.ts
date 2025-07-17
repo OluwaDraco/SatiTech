0893057cc8b9e1946ee2464178e159cebd052937
Olushola Oludipe <shotown101@gmail.com>
2025-07-17 16:25:01 -0400

    basic implementation of websocket

---
+++ sati/webSocket/types/types.d.ts
@@ -0,0 +1,8 @@
+import { Job } from "./jobType";
+export interface ServerToClientEvents {
+    serverMsg: (data: { jobs: Job[] }) => void;
+}
+
+export interface ClientToServerEvents {
+    clientMsg: (data: { jobs: Job[] }) => void;
+}
