a12b97727d272e37e2ace1b36bc99ef1b64d2b02
Olushola Oludipe <shotown101@gmail.com>
2026-04-22 12:31:17 -0400

    merged dependencies and cleaned up

---
+++ sati/Backend/jest.config.js
@@ -0,0 +1,27 @@
+export default {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+  testMatch: [
+    '**/tests/**/*.test.ts',
+  ],
+  collectCoverageFrom: [
+    'schema/**/*.ts',
+    'utils/**/*.ts',
+  ],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/generated/',
+  ],
+};
