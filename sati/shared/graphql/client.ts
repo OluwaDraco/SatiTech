20efea7b2c66c44b84f85560740f7a368a8248e0
Olushola Oludipe <shotown101@gmail.com>
2026-08-03 16:33:42 -0400

    updated client.ts to use the railway api

--- sati/shared/graphql/client.ts
+++ sati/shared/graphql/client.ts
@@ -1,7 +1,8 @@
 import { Client, cacheExchange, fetchExchange, errorExchange } from "urql";
 
 // Default configuration - can be overridden per environment
-const DEFAULT_API_URL = process.env.NEXT_PUBLIC_GRAPHQL_URL || "http://localhost:4000";
+const DEFAULT_API_URL =
+    process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000";
 
 export const createGraphQLClient = (url: string = DEFAULT_API_URL) => {
     return new Client({
