8dd134571089770986ad1bbd1fc3a5d137809941
Olushola Oludipe <shotown101@gmail.com>
2026-08-04 14:40:50 -0400

    fixed hardcoded url

--- sati/Frontend/app/api/graphql/client.ts
+++ sati/Frontend/app/api/graphql/client.ts
@@ -1,7 +1,9 @@
 import { Client, cacheExchange, fetchExchange, errorExchange } from "urql";
 
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000";
+
 export const client = new Client({
-    url: "http://localhost:4000",
+    url: API_URL,
     exchanges: [
         cacheExchange,
         errorExchange({
