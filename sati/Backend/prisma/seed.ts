3a0904862846cb889f0003e8a27df36fa5031178
Olushola Oludipe <shotown101@gmail.com>
2026-08-06 17:03:19 -0400

    updated seed file and start script

--- sati/Backend/prisma/seed.ts
+++ sati/Backend/prisma/seed.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from "../generated/prisma/index.js";
+import { randomUUID } from "crypto";
 
 const prisma = new PrismaClient();
 
@@ -118,6 +119,46 @@ async function main() {
                 admin: true,
             },
         ],
+    });
+
+    await prisma.clients.create({
+        data: {
+            id: randomUUID(),
+            company_name: "shotown",
+            country: "USA",
+            total_spent: 3000.24,
+            feedback_score: 4.5,
+            jobs: {
+                create: [
+                    {
+                        id: randomUUID(),
+                        title: "Fullstack Developer",
+                        description: "Build and maintain fullstack web apps.",
+                        skills: ["React", "Node.js"],
+                        job_type: "Full-time",
+                        budget: 3000.0,
+                        duration: "1 month",
+                        workload: "40 hrs/week",
+                        rate: 90.0,
+                        created_at: new Date(),
+                        user_id: null, // Optional
+                    },
+                    {
+                        id: randomUUID(),
+                        title: "UX/UI Designer",
+                        description: "Redesign our SaaS product interface.",
+                        skills: ["Figma", "UX Research"],
+                        job_type: "Freelance",
+                        budget: 1200.0,
+                        duration: "2 weeks",
+                        workload: "10 hrs/week",
+                        rate: 65.0,
+                        created_at: new Date(),
+                        user_id: null, // Optional
+                    },
+                ],
+            },
+        },
     });
 
     // sati_users
