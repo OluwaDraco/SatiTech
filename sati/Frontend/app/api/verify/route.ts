eaa79333ba596f425fa00ac0c317474097d7c1eb
Olushola Oludipe <shotown101@gmail.com>
2026-08-06 22:05:15 -0400

    wip

--- sati/Frontend/app/api/verify/route.ts
+++ sati/Frontend/app/api/verify/route.ts
@@ -1,6 +1,7 @@
 export const runtime = "nodejs";
+export const dynamic = "force-dynamic";
+
 import { NextRequest, NextResponse } from "next/server";
-import { cookies } from "next/headers";
 import { verifyToken } from "../../../lib/jwt";
 
 export async function GET(req: NextRequest) {
